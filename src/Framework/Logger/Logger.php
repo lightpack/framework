43be1bcff6ecb9605576eb4f4eb534a8ea99199e
Pradeep Tiwari <pt21388@gmail.com>
2021-03-28 23:00:51 +0530

    Implemented simple file logger with tests

--- src/Framework/Logger/Logger.php
+++ src/Framework/Logger/Logger.php
@@ -2,19 +2,57 @@
 
 namespace Lightpack\Logger;
 
+use Psr\Log\LogLevel;
 use Psr\Log\LoggerInterface;
 
-class Logger
+class Logger implements LoggerInterface
 {
-    private $logger;
-
-    public function __construct(LoggerInterface $logger) 
+    public function __construct(string $filename) 
     {
-        $this->logger = $logger;
+        $this->filename = $filename;
+    }
+    public function emergency($message, array $context = [])
+    {
+        $this->log(LogLevel::EMERGENCY, $message, $context);
     }
 
-    public function log(string $level, string $message, array $context = [])
+    public function alert($message, array $context = [])
     {
-        $this->logger->log($level, $message, $context);
+        $this->log(LogLevel::ALERT, $message, $context);
+    }
+
+    public function critical($message, array $context = [])
+    {
+        $this->log(LogLevel::CRITICAL, $message, $context);
+    }
+
+    public function error($message, array $context = [])
+    {
+        $this->log(LogLevel::ERROR, $message, $context);
+    }
+
+    public function warning($message, array $context = [])
+    {
+        $this->log(LogLevel::WARNING, $message, $context);
+    }
+
+    public function notice($message, array $context = [])
+    {
+        $this->log(LogLevel::NOTICE, $message, $context);
+    }
+
+    public function info($message, array $context = [])
+    {
+        $this->log(LogLevel::INFO, $message, $context);
+    }
+
+    public function debug($message, array $context = [])
+    {
+        $this->log(LogLevel::DEBUG, $message, $context);
+    }
+    
+    public function log($level, $message, array $context = [])
+    {
+        file_put_contents($this->filename, "[$level] " . $message, LOCK_EX | FILE_APPEND);
     }
 }
