8cb21a437111a2aee30a7245834f06b15699f352
Pradeep Tiwari <pt21388@gmail.com>
2021-06-30 18:34:37 +0530

    Correct test configuration for database

--- src/Tests/Database/tmp/mysql.config.php
+++ src/Tests/Database/tmp/mysql.config.php
@@ -3,8 +3,8 @@
 return [
     'host' => 'localhost',
     'port' => 3306,
-    'username' => 'phpmyadmin',
-    'password' => 'phpmyadmin',
+    'username' => 'root',
+    'password' => '',
     'database' => 'test_lightpack',
     'options' => null,
 ];
