c74b7372cbbe20188c0c48ce8b156fae0624701b
Pradeep Tiwari <pt21388@gmail.com>
2021-07-19 11:57:53 +0530

    Add HTML UI for dumper

--- src/Framework/Debug/Dumper.php
+++ src/Framework/Debug/Dumper.php
@@ -6,28 +6,40 @@ class Dumper
 {
     public function varDump(...$args)
     {
-        $this->dump(function ($data) {
-            var_dump($data);
-        }, ...$args);
+        $this->dump('var_dump', $args);
     }
 
     public function printDump(...$args)
     {
-        $this->dump(function ($data) {
-            return print_r($data, 1);
-        }, ...$args);
+        $this->dump('print_r', $args);
     }
 
-    private function dump(callable $cb, ...$args)
+    private function dump($dumpFunction, $args)
     {
         if (!get_env('APP_DEBUG', true)) {
             return;
         }
 
-        foreach ($args as $arg) {
-            echo '<pre>', $cb($arg), '</pre>';
+        $template = __DIR__ . '/templates/http/dumper.php';
+
+        $this->render($template, [
+            'args' => $args[0],
+            'dump_function' => $dumpFunction,
+        ]);
+    }
+
+    private function render($template, $data)
+    {
+        // Clean the output buffer first.
+        while (\ob_get_level() !== 0) {
+            \ob_end_clean();
         }
 
-        die;
+        extract($data);
+        ob_start();
+        require $template;
+        echo ob_get_clean();
+        \flush();
+        exit();
     }
 }
