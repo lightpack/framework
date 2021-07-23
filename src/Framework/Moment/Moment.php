262674cb29523bd9d424e815efb9d9d56fa3046e
Pradeep Tiwari <pt21388@gmail.com>
2021-07-23 17:34:32 +0530

    Better comments for moment library

--- src/Framework/Moment/Moment.php
+++ src/Framework/Moment/Moment.php
@@ -2,196 +2,217 @@
 
 namespace Lightpack\Moment;
 
+use DateTime;
+
 /**
  * Simple datetime utility class.
  */
 class Moment
 {
-    public static function today(string $format = 'Y-m-d'): string
-    {
-        $date = new \DateTime('today');
+    private static $format = 'Y-m-d H:i:s';
 
-        return $date->format($format);
+    public static function format(string $format): void
+    {
+        self::$format = $format;
     }
 
-    public static function tomorrow(string $format = 'Y-m-d'): string
+    public static function today(string $format = null): string
     {
-        $date = new \DateTime('tomorrow');
+        $datetime = new DateTime('today');
 
-        return $date->format($format);
+        return $datetime->format($format ?? self::$format);
     }
 
-    public static function yesterday(string $format = 'Y-m-d'): string
+    public static function tomorrow(string $format = null): string
     {
-        $date = new \DateTime('yesterday');
+        $datetime = new DateTime('tomorrow');
 
-        return $date->format($format);
+        return $datetime->format($format ?? self::$format);
     }
 
-    public static function this(string $day, string $format = 'Y-m-d'): string
+    public static function yesterday(string $format = null): string
     {
-        $date = new \DateTime('this ' . strtolower($day));
+        $datetime = new DateTime('yesterday');
 
-        return $date->format($format);
+        return $datetime->format($format ?? self::$format);
     }
 
-    public static function next(string $day, string $format = 'Y-m-d'): string
+    public static function this(string $day, string $format = null): string
     {
-        $date = new \DateTime('next ' . strtolower($day));
+        $datetime = new DateTime('this ' . strtolower($day));
 
-        return $date->format($format);
+        return $datetime->format($format ?? self::$format);
     }
 
-    public static function last(string $day, string $format = 'Y-m-d'): string
+    public static function next(string $day, string $format = null): string
     {
-        $date = new \DateTime('last ' . strtolower($day));
+        $datetime = new DateTime('next ' . strtolower($day));
 
-        return $date->format($format);
+        return $datetime->format($format ?? self::$format);
     }
 
-    public static function previous(string $day, string $format = 'Y-m-d'): string
+    public static function last(string $day, string $format = null): string
     {
-        $date = new \DateTime('previous ' . strtolower($day));
+        $datetime = new DateTime('last ' . strtolower($day));
 
-        return $date->format($format);
+        return $datetime->format($format ?? self::$format);
     }
 
-    public static function ago(int $days, string $format = 'Y-m-d'): string
+    public static function previous(string $day, string $format = null): string
     {
-        $date = new \DateTime("-{$days} days");
+        $datetime = new DateTime('previous ' . strtolower($day));
 
-        return $date->format($format);
+        return $datetime->format($format ?? self::$format);
     }
 
-    public static function after(int $days, string $format = 'Y-m-d'): string
+    public static function ago(int $days, string $format = null): string
     {
-        $date = new \DateTime("+{$days} days");
+        $datetime = new DateTime("-{$days} days");
 
-        return $date->format($format);
+        return $datetime->format($format ?? self::$format);
     }
 
-    public static function  endOfThisMonth(string $format = 'Y-m-d'): string
+    public static function after(int $days, string $format = null): string
     {
-        $date = new \DateTime('last day of this month');
+        $datetime = new DateTime("+{$days} days");
 
-        return $date->format($format);
+        return $datetime->format($format ?? self::$format);
     }
 
-    public static function  endOfNextMonth(string $format = 'Y-m-d'): string
+    public static function  endOfThisMonth(string $format = null): string
     {
-        $date = new \DateTime('last day of next month');
+        $datetime = new DateTime('last day of this month');
 
-        return $date->format($format);
+        return $datetime->format($format ?? self::$format);
     }
 
-    public static function  endOfPreviousMonth(string $format = 'Y-m-d'): string
+    public static function  endOfNextMonth(string $format = null): string
     {
-        $date = new \DateTime('last day of previous month');
+        $datetime = new DateTime('last day of next month');
 
-        return $date->format($format);
+        return $datetime->format($format ?? self::$format);
     }
 
-    public static function add(string $date, int $days, string $format = 'Y-m-d'): string
+    public static function  endOfPreviousMonth(string $format = null): string
     {
-        $date = new \DateTime($date);
+        $datetime = new DateTime('last day of previous month');
 
-        return $date->modify("+$days days")->format($format);
+        return $datetime->format($format ?? self::$format);
     }
 
-    public static function remove(string $date, int $days, string $format = 'Y-m-d'): string
+    public static function add(string $datetime, int $days, string $format = null): string
     {
-        $date = new \DateTime($date);
+        $datetime = new DateTime($datetime);
 
-        return $date->modify("-$days days")->format($format);
+        return $datetime->modify("+$days days")->format($format ?? self::$format);
     }
 
-    public static function diff(string $date1, string $date2): \DateInterval
+    public static function remove(string $datetime, int $days, string $format = null): string
     {
-        $datetime1 = new \DateTime($date1);
-        $datetime2 = new \DateTime($date2);
+        $datetime = new DateTime($datetime);
 
-        return $datetime1->diff($datetime2, true);
+        return $datetime->modify("-$days days")->format($format ?? self::$format);
     }
 
-    public static function daysBetween(string $date1, string $date2): int
+    public static function diff(string $datetime1, string $datetime2): \DateInterval
     {
-        return self::diff($date1, $date2)->days;
+        $datetimetime1 = new DateTime($datetime1);
+        $datetimetime2 = new DateTime($datetime2);
+
+        return $datetimetime1->diff($datetimetime2, true);
     }
 
-    public static function fromNow(string $date): ?string
+    public static function daysBetween(string $datetime1, string $datetime2): int
     {
-        $datetime1 = new \DateTime();
-        $datetime2 = new \DateTime($date);
+        return self::diff($datetime1, $datetime2)->days;
+    }
+
+    public static function now($format = null): string
+    {
+        $datetime = new DateTime('now');
+
+        return $datetime->format($format ?? self::$format);
+    }
+
+    public static function create(string $datetime = 'now'): DateTime
+    {
+        return new DateTime($datetime);
+    }
+
+    public static function fromNow(string $datetime): ?string
+    {
+        $datetimetime1 = new DateTime();
+        $datetimetime2 = new DateTime($datetime);
 
         // Seconds from now
-        $seconds = $datetime1->getTimestamp() - $datetime2->getTimestamp();
+        $seconds = $datetimetime1->getTimestamp() - $datetimetime2->getTimestamp();
 
-        if($seconds <= 60) {
+        if ($seconds <= 60) {
             return 'just now';
         }
 
         // Minutes from now
         $minutes = round($seconds / 60);
-        
-        if($minutes == 1) {
+
+        if ($minutes == 1) {
             return 'a minute ago';
         }
 
-        if($minutes <= 60) {
+        if ($minutes <= 60) {
             return "{$minutes} minutes ago";
         }
 
         // Hours from now
         $hours = round($seconds / 3600);
 
-        if($hours == 1) {
+        if ($hours == 1) {
             return 'an hour ago';
         }
 
-        if($hours <= 24) {
+        if ($hours <= 24) {
             return "{$hours} hours ago";
         }
 
         // Days from now
         $days = round($seconds / 86400);
 
-        if($days == 1) {
+        if ($days == 1) {
             return 'yesterday';
         }
 
-        if($days <= 7) {
+        if ($days <= 7) {
             return "{$days} days ago";
         }
 
         // Weeks from now
         $weeks = round($seconds / 604800);
 
-        if($weeks == 1) {
+        if ($weeks == 1) {
             return 'a week ago';
         }
 
-        if($weeks <= 4.3) {
+        if ($weeks <= 4.3) {
             return "{$weeks} weeks ago";
         }
-        
+
         // Months from now
         $months = round($seconds / 2600640);
 
-        if($months == 1) {
+        if ($months == 1) {
             return 'a month ago';
         }
 
-        if($months <= 12) {
+        if ($months <= 12) {
             return "{$months} months ago";
         }
 
         // Years from now
         $years = round($seconds / 31207680);
 
-        if($years == 1) {
+        if ($years == 1) {
             return 'a year ago';
         }
-        
+
         return "{$years} years ago";
     }
 }
