b060b9aa8737c2ff44457988a5d7cc5bd52ee229
Pradeep Tiwari <pt21388@gmail.com>
2021-08-06 13:32:05 +0530

    Initial draft for migrations helper

---
+++ src/Framework/Database/Migrations/Migrator.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Lightpack\Database\Migrations;
+
+use Lightpack\Database\Pdo;
+use Lightpack\File\File;
+
+/*
+CREATE TABLE migrations (
+    id int NOT NULL AUTO_INCREMENT,
+    migration VARCHAR(255),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+); 
+*/
+class Migrator
+{
+    /**
+     * @var \Lightpack\Database\Pdo
+     */
+    private $connection;
+
+    public function __construct(Pdo $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function run(string $path)
+    {
+        $migrationFiles = (new File)->traverse($path);
+
+        $allMigrations = array_keys($migrationFiles);
+        $executedMigrations = $this->getExecutedMigrations();
+        $migrations = array_diff($allMigrations, $executedMigrations);
+
+        ksort($migrations);
+
+        foreach ($migrations as $migration) {
+            $migrationFile = $migrationFiles[$migration];
+            $migrationFilepath = $migrationFile->getPathname();
+
+            // Execute migration
+            $sql = file_get_contents($migrationFilepath);
+            $this->connection->query($sql);  
+
+            // Record migration
+            $sql = "INSERT INTO migrations (migration) VALUES ('{$migration}');";
+            $this->connection->query($sql);
+        }
+    }
+
+    public function rollback($path, int $steps = null)
+    {
+        $migrationFiles = (new File)->traverse($path);
+
+        $migrations = array_keys($migrationFiles);
+
+        ksort($migrations);
+
+        foreach ($migrations as $migration) {
+            $migrationFile = $migrationFiles[$migration];
+            $migrationFilepath = $migrationFile->getPathname();
+
+            // Execute migration
+            $sql = file_get_contents($migrationFilepath);
+            $this->connection->query($sql);  
+        }
+    }
+
+    private function getExecutedMigrations()
+    {
+        $migrations = [];
+        $rows = $this->connection->query('SELECT migration FROM migrations')->fetchAll();
+        
+        foreach($rows as $row) {
+            $migrations[] = $row['migration'];
+        }
+
+        return $migrations;
+    }
+}
