12e6ef654c3907b18db44e344f0d8c3b26c40242
Pradeep Tiwari <pt21388@gmail.com>
2022-04-23 13:59:56 +0530

    Added tests for model

---
+++ src/Tests/Database/Lucid/CollectionTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class CollectionTest extends TestCase
+{
+    public function setUp(): void
+    {
+        //
+    }
+
+    public function testIsJsonSerializable()
+    {
+        //
+    }
+
+    public function testContructor()
+    {
+        //
+    }
+}
