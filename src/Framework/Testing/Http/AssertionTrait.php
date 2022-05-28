81974a57f91d4fd79d844187a7ddfaab75e4a135
Pradeep Tiwari <pt21388@gmail.com>
2022-05-28 12:52:31 +0530

    Base http test case

---
+++ src/Framework/Testing/Http/AssertionTrait.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Lightpack\Testing\Http;
+
+use Lightpack\Exceptions\RouteNotFoundException;
+
+trait AssertionTrait
+{
+    public function assertResponseStatus(int $code)
+    {
+        $this->assertEquals($code, $this->response->getCode());
+    }
+
+    public function assertResponseBody(string $body)
+    {
+        $this->assertEquals($body, $this->response->getBody());
+    }
+
+    public function assertResponseHasValidJson()
+    {
+        $this->assertJson($this->response->getBody());
+    }
+
+    public function assertResponseJson(array $json)
+    {
+        $this->assertEquals($json, json_decode($this->response->getBody(), true));
+    }
+
+    public function assertResponseHasHeader(string $header)
+    {
+        $this->assertTrue($this->response->hasHeader($header));
+    }
+
+    public function assertRedirectUrl(string $url)
+    {
+        $this->assertEquals($url, $this->response->getRedirectUrl());
+    }
+
+    public function assertResponseHeaderEquals(string $header, string $value)
+    {
+        $this->assertEquals($value, $this->response->getHeader($header));
+    }
+
+    public function assertRouteNotFound(string $route)
+    {
+        $this->expectException(RouteNotFoundException::class);
+
+        $this->request('GET', $route);
+    }
+}
--- src/Framework/Testing/Http/TestCase.php
+++ src/Framework/Testing/Http/TestCase.php
@@ -1,13 +1,14 @@
 <?php
 
-namespace Lightpack\Testing;
+namespace Lightpack\Testing\Http;
 
 use Lightpack\Http\Response;
 use PHPUnit\Framework\TestCase as BaseTestCase;
-use Lightpack\Exceptions\RouteNotFoundException;
 
-class HttpTestCase extends BaseTestCase
+class TestCase extends BaseTestCase
 {
+    use AssertionTrait;
+
     /** @var \Lightpack\Container\Container */
     protected $container;
 
@@ -29,7 +30,7 @@ class HttpTestCase extends BaseTestCase
         if ($this->isJsonRequest) {
             $_SERVER['X_LIGHTPACK_RAW_INPUT'] = json_encode($params);
         }
-        
+
         if ($this->isMultipartFormdata) {
             $_SERVER['X_LIGHTPACK_TEST_UPLOAD'] = true;
         }
@@ -111,51 +112,9 @@ class HttpTestCase extends BaseTestCase
         return json_decode($this->response->getBody(), true);
     }
 
-    public function assertResponseStatus(int $code)
-    {
-        $this->assertEquals($code, $this->response->getCode());
-    }
-
-    public function assertResponseBody(string $body)
-    {
-        $this->assertEquals($body, $this->response->getBody());
-    }
-
-    public function assertResponseHasValidJson()
-    {
-        $this->assertJson($this->response->getBody());
-    }
-
-    public function assertResponseJson(array $json)
-    {
-        $this->assertEquals($json, json_decode($this->response->getBody(), true));
-    }
-
-    public function assertResponseHasHeader(string $header)
-    {
-        $this->assertTrue($this->response->hasHeader($header));
-    }
-
-    public function assertRedirectUrl(string $url)
-    {
-        $this->assertEquals($url, $this->response->getRedirectUrl());
-    }
-
-    public function assertResponseHeaderEquals(string $header, string $value)
-    {
-        $this->assertEquals($value, $this->response->getHeader($header));
-    }
-
-    public function assertRouteNotFound(string $route)
-    {
-        $this->expectException(RouteNotFoundException::class);
-
-        $this->request('GET', $route);
-    }
-
     protected function setRequestContentType()
     {
-        if($this->isMultipartFormdata) {
+        if ($this->isMultipartFormdata) {
             $_SERVER['CONTENT_TYPE'] = 'multipart/form-data';
             return;
         }
