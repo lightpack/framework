77dd82abddb9bb957d4d8d3810524700a9e742f3
Pradeep Tiwari <pt21388@gmail.com>
2022-09-24 15:22:45 +0530

    Refactoring utilitites

--- src/Framework/Logger/Drivers/FileLogger.php
+++ src/Framework/Logger/Drivers/FileLogger.php
@@ -16,6 +16,11 @@ class FileLogger implements ILogger
     public function log($level, $message, array $context = [])
     {
         $content = date('Y-m-d H:i:s') . " $level : " . $message . PHP_EOL;
+
+        if($context) {
+            $content .= 'Context: ' . json_encode($context) . PHP_EOL;
+        }
+
         file_put_contents($this->filename, $content, LOCK_EX | FILE_APPEND);
     }
 }
