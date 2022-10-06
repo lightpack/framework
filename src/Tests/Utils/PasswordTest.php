99485575dba7cc5053eaed51e874ae5b8a70afa0
Pradeep Tiwari <pt21388@gmail.com>
2022-10-06 13:13:55 +0530

    Password generator and strength checker

--- src/Tests/Utils/PasswordTest.php
+++ src/Tests/Utils/PasswordTest.php
@@ -14,4 +14,34 @@ final class PasswordTest extends TestCase
 
         $this->assertTrue($verified);
     }
+
+    public function testGenerateRandomPassword()
+    {
+        $password = new Password();
+        $pass1 = $password->generate();
+        $pass2 = $password->generate(16);
+
+        $this->assertEquals(8, strlen($pass1));
+        $this->assertEquals(16, strlen($pass2));
+
+        // expect exception
+        $this->expectException(\Exception::class);
+        $password->generate(5);
+    }
+
+    public function testPasswordStrength()
+    {
+        $password = new Password();
+
+        $this->assertEquals('weak', $password->strength('123456'));
+        $this->assertEquals('weak', $password->strength('12345678'));
+        $this->assertEquals('weak', $password->strength('abcdefgh'));
+        $this->assertEquals('weak', $password->strength('ABCDEFGH'));
+        $this->assertEquals('medium', $password->strength('abc123ABC'));
+        $this->assertEquals('medium', $password->strength('abcdefgh123456'));
+        $this->assertEquals('medium', $password->strength('ABCDEFGHabcdefgh123456'));
+        $this->assertEquals('strong', $password->strength('A123#abc'));
+        $this->assertEquals('strong', $password->strength('%1BCD23c'));
+        $this->assertEquals('strong', $password->strength('12345678%1BCD23c'));
+    }
 }
