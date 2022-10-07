ee50954c026522936892361b8bd1574812c05525
Pradeep Tiwari <pt21388@gmail.com>
2022-10-07 12:09:11 +0530

    Refactored app boot process

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -5,49 +5,115 @@ namespace Lightpack;
 use Lightpack\Http\Response;
 use Lightpack\Routing\Dispatcher;
 use Lightpack\Container\Container;
+use Lightpack\Debug\ExceptionRenderer;
+use Lightpack\Debug\Handler;
+use Lightpack\Exceptions\FilterNotFoundException;
 
 final class App
 {
-    public static function run(Container $container): Response 
+    public static function bootProviders(array $providers = [])
     {
-        /**
-         * Prepare variables. 
-         */
+        $providers = self::getFrameworkProviders() + $providers;
+
+        foreach ($providers as $provider) {
+            $provider = new $provider();
+            $provider->register(Container::getInstance());
+        }
+    }
+
+    public static function bootEvents()
+    {
+        $container = Container::getInstance();
+        $events = $container->get('config')->get('events');
+
+        foreach ($events as $event => $listeners) {
+            foreach ($listeners as $listener) {
+                $container->get('event')->subscribe($event, $listener);
+            }
+        }
+    }
+
+    public static function bootDebugHandler()
+    {
+        $container = Container::getInstance();
+        $logger = $container->get('logger');
+        $environment = $container->get('config')->get('app.env');
+        $exceptionRenderer = new ExceptionRenderer($environment);
+        $handler = new Handler($logger, $exceptionRenderer);
+
+        set_exception_handler([$handler, 'handleException']);
+        set_error_handler([$handler, 'handleError']);
+        register_shutdown_function([$handler, 'handleShutdown']);
+    }
+
+    public static function run(): Response
+    {
+        $container = Container::getInstance();
         $response = $container->get('response');
         $filter = $container->get('filter');
         $dispatcher = new Dispatcher($container);
         $route = $container->get('router')->route();
 
-        /**
-         * Boot app filters.
-         */
-        require_once DIR_BOOTSTRAP . '/filters.php';
+        self::bootFilters();
 
-        /**
-         * Process before filters.
-         */
+        // Process before filters.
         $result = $filter->processBeforeFilters($route);
-        
-        if($result instanceof Response) {
-            // $result->send();
+
+        if ($result instanceof Response) {
             return $result;
         }
 
-        /**
-         * Dispatch app request.
-         */
+        // Dispatch app request.
         $result = $dispatcher->dispatch();
 
-        if($result instanceof Response) {
+        if ($result instanceof Response) {
             $response = $result;
         }
 
-        /**
-         * Process after filters.
-         */
+        // Process after filters.
         $filter->setResponse($response);
         $response = $filter->processAfterFilters($route);
 
         return $response;
+    }
+
+    private static function getFrameworkProviders(): array
+    {
+        return [
+            \Lightpack\Providers\LogProvider::class,
+            \Lightpack\Providers\RouteProvider::class,
+            \Lightpack\Providers\EventProvider::class,
+            \Lightpack\Providers\CacheProvider::class,
+            \Lightpack\Providers\ConfigProvider::class,
+            \Lightpack\Providers\RouterProvider::class,
+            \Lightpack\Providers\FilterProvider::class,
+            \Lightpack\Providers\CookieProvider::class,
+            \Lightpack\Providers\SessionProvider::class,
+            \Lightpack\Providers\RequestProvider::class,
+            \Lightpack\Providers\ResponseProvider::class,
+            \Lightpack\Providers\DatabaseProvider::class,
+            \Lightpack\Providers\TemplateProvider::class,
+            \Lightpack\Providers\AuthProvider::class,
+            \Lightpack\Providers\CryptoProvider::class,
+        ];
+    }
+
+    private static function bootFilters()
+    {
+        $container = Container::getInstance();
+        $filtersConfig = $container->get('config')->get('filters');
+        $routeFilters = $container->get('router')->filters();
+        $filter = $container->get('filter');
+        $router = $container->get('router');
+
+        foreach ($routeFilters as $filterAlias) {
+            if (!array_key_exists($filterAlias, $filtersConfig)) {
+                throw new FilterNotFoundException(
+                    "No filter class registered for: {$filterAlias}"
+                );
+            }
+
+            $filter->register($router->route(), new $filtersConfig[$filterAlias]);
+        }
     }
 }
