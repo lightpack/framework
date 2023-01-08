9c8d9de5f6cd2632ef8aadbe6f388187d9fa048f
Pradeep Tiwari <pt21388@gmail.com>
2023-01-08 08:59:47 +0530

    Log seeded class output to console

---
+++ src/Framework/Database/Seeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Lightpack\Database;
+
+use Lightpack\Container\Container;
+
+class Seeder
+{
+    public function run(string|array $seeder)
+    {
+        if (!is_array($seeder)) {
+            $seeder = [$seeder];
+        }
+
+        fputs(STDOUT, "\nRunning seeders...\n\n");
+
+        foreach ($seeder as $class) {
+            Container::getInstance()->call($class, 'seed');
+            fputs(STDOUT, "Seeded: {$class}\n");
+        }
+    }
+}
