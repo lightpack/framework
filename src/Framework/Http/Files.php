f1d6ec3b97f39c79d2a32c88c6e662c271ec3a5b
Pradeep Tiwari <pt21388@gmail.com>
2023-06-12 11:24:35 +0530

    Added methods to check if files were uploaded

--- src/Framework/Http/Files.php
+++ src/Framework/Http/Files.php
@@ -31,6 +31,44 @@ class Files
         return isset($this->files[$key]);
     }
 
+    /**
+     * Checks if the file or files associated with the given key are empty. An empty 
+     * file typically means that no file was selected for uploading.
+     *
+     * @param string $key The key associated with the file or files to be checked.
+     * @return bool Returns true if the file or files are not empty, false otherwise.
+     */
+    public function isEmpty(string $key): bool
+    {
+        return $this->isNotEmpty($key) == false;
+    }
+
+    /**
+      * Checks if the file or files associated with the given key are not empty. An empty 
+     * file typically means that no file was selected for uploading.
+     *
+     * @param string $key The key associated with the file or files to be checked.
+     * @return bool Returns true if the file or files are not empty, false otherwise.
+     */
+    public function isNotEmpty(string $key): bool
+    {
+        $file = $this->get($key);
+        
+        if ($file instanceof UploadedFile) {
+            return $file->getError() != UPLOAD_ERR_NO_FILE;
+        }
+
+        if (is_array($file)) {
+            foreach ($file as $uploadedFile) {
+                if ($uploadedFile->getError() == UPLOAD_ERR_NO_FILE) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
     private function populateUploadedFiles(array $files)
     {
         $uploads = [];
--- src/Framework/Providers/ValidationProvider.php
+++ src/Framework/Providers/ValidationProvider.php
@@ -10,6 +10,9 @@ class ValidationProvider implements ProviderInterface
     public function register(Container $container)
     {
         $container->factory('validator', function ($container) {
+            $data = $container->get('request')->input();
+            $data = array_merge($data, $container->get('request')->files()->get());
+
             return (new Validator)->setInput($container->get('request')->input());
         });
 
