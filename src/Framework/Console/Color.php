04067d0b9b2a3715802d690c4064bb5b9cf701b2
Pradeep Tiwari <pt21388@gmail.com>
2023-06-21 11:13:52 +0530

    Added color utility

--- src/Framework/Console/Color.php
+++ src/Framework/Console/Color.php
@@ -4,80 +4,82 @@ namespace Lightpack\Console;
 
 class Color
 {
-    const RED = "\033[0;31m";
-    const GREEN = "\033[0;32m";
-    const YELLOW = "\033[0;33m";
-    const BLUE = "\033[0;34m";
     const RESET = "\033[0m";
-    
-    const BG_RED = "\033[41m";
-    const BG_GREEN = "\033[42m";
-    const BG_YELLOW = "\033[43m";
-    const BG_BLUE = "\033[44m";
-    const BG_RESET = "\033[49m";
-    
-    const RESET_LABEL = '<RESET>';   // Adjust the reset label as needed
+
+    const COLORS = [
+        'RED' => "\033[0;31m",
+        'GREEN' => "\033[0;32m",
+        'YELLOW' => "\033[0;33m",
+        'BLUE' => "\033[0;34m"
+    ];
+
+    const BG_COLORS = [
+        'BG_RED' => "\033[41m",
+        'BG_GREEN' => "\033[42m",
+        'BG_YELLOW' => "\033[43m",
+        'BG_BLUE' => "\033[44m"
+    ];
 
     public function format($string)
     {
-        $string = preg_replace_callback('/<(\w+)>/', function ($matches) {
-            $label = strtoupper($matches[1]);
-            if (defined('self::' . $label)) {
-                return constant('self::' . $label);
+        $string = preg_replace_callback('/<(\w+)>(.*?)<\/\1>/i', function ($matches) {
+            $tag = strtoupper($matches[1]);
+            if (array_key_exists($tag, self::COLORS)) {
+                return self::COLORS[$tag] . $matches[2] . self::RESET;
+            } elseif (array_key_exists($tag, self::BG_COLORS)) {
+                return self::BG_COLORS[$tag] . $matches[2] . self::RESET;
             }
             return $matches[0];
         }, $string);
-
-        $string = str_replace(self::RESET_LABEL, self::RESET . self::BG_RESET, $string);
 
         return $string;
     }
 
     public function info(string $text)
     {
-        return Color::format("<BLUE>{$text}<RESET>");
+        return Color::format("<BLUE>{$text}</BLUE>");
     }
 
     public function success(string $text)
     {
-        return Color::format("<GREEN>{$text}<RESET>");
+        return Color::format("<GREEN>{$text}</GREEN>");
     }
 
     public function error(string $text)
     {
-        return Color::format("<RED>{$text}<RESET>");
+        return Color::format("<RED>{$text}</RED>");
     }
 
     public function warning(string $text)
     {
-        return Color::format("<YELLOW>{$text}<RESET>");
+        return Color::format("<YELLOW>{$text}</YELLOW>");
     }
 
     public function infoLabel(?string $text = null)
     {
         $text = $text ?? ' INFO ';
 
-        return Color::format("<BG_BLUE>{$text}<BG_RESET>");
+        return Color::format("<BG_BLUE>{$text}</BG_BLUE>");
     }
 
     public function successLabel(?string $text = null)
     {
         $text = $text ?? ' SUCCESS ';
 
-        return Color::format("<BG_GREEN>{$text}<BG_RESET>");
+        return Color::format("<BG_GREEN>{$text}</BG_GREEN>");
     }
 
     public function errorLabel(?string $text = null)
     {
         $text = $text ?? ' ERROR ';
 
-        return Color::format("<BG_RED>{$text}<BG_RESET>");
+        return Color::format("<BG_RED>{$text}</BG_RED>");
     }
 
     public function warningLabel(?string $text = null)
     {
         $text = $text ?? ' WARNING ';
 
-        return Color::format("<BG_YELLOW>{$text}<BG_RESET>");
+        return Color::format("<BG_YELLOW>{$text}</BG_YELLOW>");
     }
 }
--- src/Tests/Console/ColorTest.php
+++ src/Tests/Console/ColorTest.php
@@ -1,31 +1,38 @@
 <?php
 
 use Lightpack\Console\Color;
+use PHPUnit\Framework\TestCase;
 
-class ColorTest extends PHPUnit\Framework\TestCase
+class ColorTest extends TestCase
 {
     public function testForegroundColors()
     {
-        $this->assertEquals("\033[0;31mHello\033[0m", (new Color)->format('<RED>Hello<RESET>'));
-        $this->assertEquals("\033[0;32mHello\033[0m", (new Color)->format('<GREEN>Hello<RESET>'));
-        $this->assertEquals("\033[0;33mHello\033[0m", (new Color)->format('<YELLOW>Hello<RESET>'));
-        $this->assertEquals("\033[0;34mHello\033[0m", (new Color)->format('<BLUE>Hello<RESET>'));
+        $this->assertEquals("\033[0;31mHello\033[0m", (new Color)->format('<RED>Hello</RED>'));
+        $this->assertEquals("\033[0;32mHello\033[0m", (new Color)->format('<GREEN>Hello</GREEN>'));
+        $this->assertEquals("\033[0;33mHello\033[0m", (new Color)->format('<YELLOW>Hello</YELLOW>'));
+        $this->assertEquals("\033[0;34mHello\033[0m", (new Color)->format('<BLUE>Hello</BLUE>'));
     }
 
     public function testBackgroundColors()
     {
-        $this->assertEquals("\033[41mHello\033[49m", (new Color)->format('<BG_RED>Hello<BG_RESET>'));
-        $this->assertEquals("\033[42mHello\033[49m", (new Color)->format('<BG_GREEN>Hello<BG_RESET>'));
-        $this->assertEquals("\033[43mHello\033[49m", (new Color)->format('<BG_YELLOW>Hello<BG_RESET>'));
-        $this->assertEquals("\033[44mHello\033[49m", (new Color)->format('<BG_BLUE>Hello<BG_RESET>'));
+        $this->assertEquals("\033[41mHello\033[0m", (new Color)->format('<BG_RED>Hello</BG_RED>'));
+        $this->assertEquals("\033[42mHello\033[0m", (new Color)->format('<BG_GREEN>Hello</BG_GREEN>'));
+        $this->assertEquals("\033[43mHello\033[0m", (new Color)->format('<BG_YELLOW>Hello</BG_YELLOW>'));
+        $this->assertEquals("\033[44mHello\033[0m", (new Color)->format('<BG_BLUE>Hello</BG_BLUE>'));
     }
 
     public function testForegroundAndBackgroundColors()
     {
-        $this->assertEquals("\033[0;31m\033[42mHello\033[0m\033[49m", (new Color)->format('<RED><BG_GREEN>Hello<RESET><BG_RESET>'));
-        $this->assertEquals("\033[0;32m\033[41mHello\033[0m\033[49m", (new Color)->format('<GREEN><BG_RED>Hello<RESET><BG_RESET>'));
-        $this->assertEquals("\033[0;33m\033[44mHello\033[0m\033[49m", (new Color)->format('<YELLOW><BG_BLUE>Hello<RESET><BG_RESET>'));
-        $this->assertEquals("\033[0;34m\033[43mHello\033[0m\033[49m", (new Color)->format('<BLUE><BG_YELLOW>Hello<RESET><BG_RESET>'));
+        $color = new Color;
+        $red = '<RED>Hello</RED>';
+        $green = '<GREEN>Hello</GREEN>';
+        $yellow = '<YELLOW>Hello</YELLOW>';
+        $blue = '<BLUE>Hello</BLUE>';
+
+        $this->assertEquals("\033[42m\033[0;31mHello\033[0m\033[0m", $color->format('<BG_GREEN>' . $color->format($red) . '</BG_GREEN>'));
+        $this->assertEquals("\033[41m\033[0;32mHello\033[0m\033[0m", $color->format('<BG_RED>' . $color->format($green) . '</BG_RED>'));
+        $this->assertEquals("\033[44m\033[0;33mHello\033[0m\033[0m", $color->format('<BG_BLUE>' . $color->format($yellow) . '</BG_BLUE>'));
+        $this->assertEquals("\033[43m\033[0;34mHello\033[0m\033[0m", $color->format('<BG_YELLOW>' . $color->format($blue) . '</BG_YELLOW>'));
     }
 
     public function testInvalidColorLabel()
@@ -56,25 +63,25 @@ class ColorTest extends PHPUnit\Framework\TestCase
 
     public function testColorErrorLabel()
     {
-        $this->assertEquals("\033[41m ERROR \033[49m", (new Color)->errorLabel());
-        $this->assertEquals("\033[41mERROR\033[49m", (new Color)->errorLabel('ERROR'));
+        $this->assertEquals("\033[41m ERROR \033[0m", (new Color)->errorLabel());
+        $this->assertEquals("\033[41mERROR\033[0m", (new Color)->errorLabel('ERROR'));
     }
 
     public function testColorSuccessLabel()
     {
-        $this->assertEquals("\033[42m SUCCESS \033[49m", (new Color)->successLabel());
-        $this->assertEquals("\033[42mSUCCESS\033[49m", (new Color)->successLabel('SUCCESS'));
+        $this->assertEquals("\033[42m SUCCESS \033[0m", (new Color)->successLabel());
+        $this->assertEquals("\033[42mSUCCESS\033[0m", (new Color)->successLabel('SUCCESS'));
     }
 
     public function testColorWarningLabel()
     {
-        $this->assertEquals("\033[43m WARNING \033[49m", (new Color)->warningLabel());
-        $this->assertEquals("\033[43mWARNING\033[49m", (new Color)->warningLabel('WARNING'));
+        $this->assertEquals("\033[43m WARNING \033[0m", (new Color)->warningLabel());
+        $this->assertEquals("\033[43mWARNING\033[0m", (new Color)->warningLabel('WARNING'));
     }
 
     public function testColorInfoLabel()
     {
-        $this->assertEquals("\033[44m INFO \033[49m", (new Color)->infoLabel());
-        $this->assertEquals("\033[44mINFO\033[49m", (new Color)->infoLabel('INFO'));
+        $this->assertEquals("\033[44m INFO \033[0m", (new Color)->infoLabel());
+        $this->assertEquals("\033[44mINFO\033[0m", (new Color)->infoLabel('INFO'));
     }
 }
