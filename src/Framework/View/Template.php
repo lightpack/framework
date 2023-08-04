42b11ab5ca58443c30e0d0ab56c937e2e7f0f3d9
Pradeep Tiwari <pt21388@gmail.com>
2023-08-04 08:40:50 +0530

    Add partial method in template

--- src/Framework/View/Template.php
+++ src/Framework/View/Template.php
@@ -4,26 +4,42 @@ namespace Lightpack\View;
 
 class Template
 {
-    private $content;
     private $data = [];
 
+    /**
+     * @deprecated Use render method to set data instead.
+     */
     public function setData(array $data = []): self
     {
         $this->data = array_merge($this->data, $data);
         return $this;
     }
 
-    public function render(string $file): string
+    public function render(string $file, array $data = []): string
     {
+        $this->data = array_merge($this->data, $data);
         $file = DIR_VIEWS . '/' . $file . '.php';
+
         $this->throwExceptionIfTemplateNotFound($file);
-        
+
         // Queue up the content in buffer.
-        ob_start();
-        extract($this->data);
-        require $file;
-        return ob_get_clean();
+        $output = (function ($data) {
+            extract($data);
+            ob_start();
+            require func_get_arg(1);
+            return ob_get_clean();
+        })($this->data, $file);
+
         flush();
+
+        return $output;
+    }
+
+    public function partial(string $file, array $data = []): string
+    {
+        $template = new self();
+
+        return $template->render($file, array_merge($this->data, $data));
     }
 
     private function throwExceptionIfTemplateNotFound(string $template)
