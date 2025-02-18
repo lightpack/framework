9551ded2416690ae89ffc1d67bace983c57249db
Pradeep Tiwari <pt21388@gmail.com>
2025-02-18 15:05:44 +0530

    removed unwanted file validations in request

--- src/Framework/Http/UploadedFile.php
+++ src/Framework/Http/UploadedFile.php
@@ -3,7 +3,6 @@
 namespace Lightpack\Http;
 
 use Lightpack\Exceptions\FileUploadException;
-use Lightpack\Validator\Validator;
 
 class UploadedFile
 {
@@ -12,17 +11,6 @@ class UploadedFile
     private $type;
     private $error;
     private $tmpName;
-    private $validation;
-    private $errors = [
-        UPLOAD_ERR_OK => 'There is no error, the file uploaded with success',
-        UPLOAD_ERR_INI_SIZE => 'The uploaded file exceeds the upload_max_filesize directive in php.ini',
-        UPLOAD_ERR_FORM_SIZE => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form',
-        UPLOAD_ERR_PARTIAL => 'The uploaded file was only partially uploaded',
-        UPLOAD_ERR_NO_FILE => 'No file was uploaded',
-        UPLOAD_ERR_NO_TMP_DIR => 'Missing a temporary folder',
-        UPLOAD_ERR_CANT_WRITE => 'Failed to write file to disk',
-        UPLOAD_ERR_EXTENSION => 'A PHP extension stopped the file upload',
-    ];
 
     public function __construct($file)
     {
@@ -31,50 +19,6 @@ class UploadedFile
         $this->type = $file['type'];
         $this->error = $file['error'];
         $this->tmpName = $file['tmp_name'];
-        $this->validation = new UploadValidation();
-    }
-
-    public function setRules(string $rules): self
-    {
-        $this->validation->setRules($rules);
-
-        return $this;
-    }
-
-    public function passedValidation(): bool
-    {
-        return $this->failedValidation() === false;
-    }
-
-    public function failedValidation(): bool
-    {
-        $this->validation->validateMimes($this->getType());
-        $this->validation->validateExtensions($this->getExtension());
-        $this->validation->validateMinWidth($this->getWidth());
-        $this->validation->validateMaxWidth($this->getWidth());
-        $this->validation->validateMinHeight($this->getHeight());
-        $this->validation->validateMaxHeight($this->getHeight());
-        $this->validation->validateRatio($this->getWidth(), $this->getHeight());
-        $this->validation->validateWidth($this->getWidth());
-        $this->validation->validateHeight($this->getHeight());
-        $this->validation->validateMinSize($this->getSize());
-        $this->validation->validateMaxSize($this->getSize());
-
-        if ($this->validation->hasError()) {
-            return true;
-        }
-
-        return false;
-    }
-
-    public function getValidationErrors()
-    {
-        return $this->validation->getErrors();
-    }
-
-    public function getValidationError(string $rule)
-    {
-        return $this->validation->getError($rule);
     }
 
     public function isImage()
@@ -113,11 +57,6 @@ class UploadedFile
         return $dimensions['height'];
     }
 
-    public function getRules()
-    {
-        return $this->validation->getRules();
-    }
-
     public function getName(): string
     {
         return $this->name;
@@ -138,11 +77,6 @@ class UploadedFile
         return pathinfo($this->name, PATHINFO_EXTENSION);
     }
 
-    public function getError(): string
-    {
-        return $this->error;
-    }
-
     public function getTmpName(): string
     {
         return $this->tmpName;
@@ -153,21 +87,8 @@ class UploadedFile
         return empty($this->getName());
     }
 
-    public function hasError(): bool
-    {
-        return UPLOAD_ERR_OK !== $this->error;
-    }
-
     public function move(string $destination, string $name = null): void
     {
-        if($this->failedValidation()) {
-            throw new FileUploadException('Uploaded file fails validation rules.');
-        }
-
-        if ($this->hasError()) {
-            throw new FileUploadException('Uploaded file has error. Error code: ' . $this->error . ' - ' . $this->errors[$this->error]);
-        }
-
         if (is_dir($destination)) {
             if (!is_writable($destination)) {
                 throw new FileUploadException('Upload directory does not have sufficient write permission: ' . $destination);
