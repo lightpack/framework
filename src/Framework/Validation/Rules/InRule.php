a97f6d516a892e4b288f4adbe9b454b9bce2ef3c
Pradeep Tiwari <pt21388@gmail.com>
2025-02-21 17:57:37 +0530

    app

--- src/Framework/Validation/Rules/InRule.php
+++ src/Framework/Validation/Rules/InRule.php
@@ -15,6 +15,15 @@ class InRule
 
     public function __invoke($value): bool
     {
+        if (is_array($value)) {
+            foreach ($value as $item) {
+                if (!in_array($item, $this->values, true)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
         return in_array($value, $this->values, true);
     }
 
