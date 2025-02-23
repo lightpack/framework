66e67a7bfc627ea7e1b1507af6ffdd2fb0292fbb
Pradeep Tiwari <pt21388@gmail.com>
2025-02-23 18:40:54 +0530

    refactor Config to use dot syntax

--- src/Framework/Config/Config.php
+++ src/Framework/Config/Config.php
@@ -2,12 +2,16 @@
 
 namespace Lightpack\Config;
 
+use Lightpack\Utils\Arr;
+
 class Config
 {
+    protected Arr $arr;
     protected $config = [];
 
     public function __construct()
     {
+        $this->arr = new Arr;
         $configs = glob(DIR_CONFIG . '/*.php');
 
         foreach ($configs as $config) {
@@ -20,13 +24,13 @@ class Config
 
     public function get(string $key, $default = null)
     {
-        return $this->config[$key] ?? $default;
+        return $this->arr->get($key, $this->config, $default);
     }
 
     public function set(string $key, $value)
     {
-        if (!isset($this->config[$key])) {
-            $this->config[$key] = $value;
+        if(!$this->arr->has($key, $this->config)) {
+            $this->arr->set($key, $value, $this->config);
         }
     }
 
