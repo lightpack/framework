6b835bc978df76a3b50c465f718163806a06cf46
Pradeep Tiwari <pt21388@gmail.com>
2025-03-01 21:23:03 +0530

    app

---
+++ src/Framework/Filters/GuestFilter.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Lightpack\Filters;
+
+use Lightpack\Http\Request;
+use Lightpack\Http\Response;
+use Lightpack\Filters\IFilter;
+
+class GuestFilter implements IFilter
+{
+    public function before(Request $request, array $params = [])
+    {
+        if (auth()->user()) {
+            return auth()->redirectLogin();
+        }
+    }
+
+    public function after(Request $request, Response $response, array $params = []): Response
+    {
+        return $response;
+    }
+}
