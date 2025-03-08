1c5a3d00fe67476d85d7d7f6d898e25dcc7cd20f
Pradeep Tiwari <pt21388@gmail.com>
2025-03-08 14:55:10 +0530

    chainable assertions

--- src/Framework/Testing/Http/AssertionTrait.php
+++ src/Framework/Testing/Http/AssertionTrait.php
@@ -8,54 +8,74 @@ use Lightpack\Exceptions\RouteNotFoundException;
 
 trait AssertionTrait
 {
-    public function assertResponseStatus(int $code)
+    public function assertResponseStatus(int $code): self
     {
         $this->assertEquals($code, $this->response->getStatus());
+
+        return $this;
     }
 
-    public function assertResponseBody(string $body)
+    public function assertResponseBody(string $body): self
     {
         $this->assertEquals($body, $this->response->getBody());
+
+        return $this;
     }
 
-    public function assertResponseHasValidJson()
+    public function assertResponseHasValidJson(): self
     {
         $this->assertJson($this->response->getBody());
+
+        return $this;
     }
 
-    public function assertResponseJson(array $json)
+    public function assertResponseJson(array $json): self
     {
         $this->assertEquals($json, json_decode($this->response->getBody(), true));
+
+        return $this;
     }
 
-    public function assertResponseJsonHasKey(string $key)
+    public function assertResponseJsonHasKey(string $key): self
     {
         $this->assertTrue((new Arr)->has($key, $this->getArrayResponse()));
+
+        return $this;
     }
 
-    public function assertResponseJsonKeyValue(string $key, $value)
+    public function assertResponseJsonKeyValue(string $key, $value): self
     {
         $this->assertSame($value, (new Arr)->get($key, $this->getArrayResponse()));
+
+        return $this;
     }
 
-    public function assertResponseHasHeader(string $header)
+    public function assertResponseHasHeader(string $header): self
     {
         $this->assertTrue($this->response->hasHeader($header));
+
+        return $this;
     }
 
-    public function assertResponseIsRedirect()
+    public function assertResponseIsRedirect(): self
     {
         $this->assertInstanceof(Redirect::class, $this->response);
+
+        return $this;
     }
 
-    public function assertRedirectUrl(string $url)
+    public function assertRedirectUrl(string $url): self
     {
         $this->assertEquals($url, $this->response->getRedirectUrl());
+
+        return $this;
     }
 
-    public function assertResponseHeaderEquals(string $header, string $value)
+    public function assertResponseHeaderEquals(string $header, string $value): self
     {
         $this->assertEquals($value, $this->response->getHeader($header));
+
+        return $this;
     }
 
     public function assertRouteNotFound(string $route)
