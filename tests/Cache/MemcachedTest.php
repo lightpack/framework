57c237c8cf6c619beb3fe43bcb922a03ec62696c
Pradeep Tiwari <pt21388@gmail.com>
2025-04-03 16:05:58 +0530

    session revamp

---
+++ tests/Cache/MemcachedTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Lightpack\Tests\Cache;
+
+use PHPUnit\Framework\TestCase;
+use Lightpack\Cache\Memcached;
+
+class MemcachedTest extends TestCase
+{
+    private Memcached $memcached;
+    
+    protected function setUp(): void
+    {
+        if (!extension_loaded('memcached')) {
+            $this->markTestSkipped('Memcached extension not installed');
+        }
+        
+        $this->memcached = new Memcached();
+    }
+    
+    public function testCanCreateMemcachedInstance()
+    {
+        $this->assertInstanceOf(\Memcached::class, $this->memcached->getClient());
+    }
+    
+    public function testCanAddServer()
+    {
+        $this->assertTrue($this->memcached->addServer('127.0.0.1', 11211));
+    }
+    
+    public function testCanAddMultipleServers()
+    {
+        $servers = [
+            ['127.0.0.1', 11211],
+            ['localhost', 11211],
+        ];
+        
+        $this->assertTrue($this->memcached->addServers($servers));
+    }
+    
+    public function testCanGetStats()
+    {
+        $stats = $this->memcached->getStats();
+        $this->assertIsArray($stats);
+    }
+    
+    public function testCanGetVersion()
+    {
+        $version = $this->memcached->getVersion();
+        $this->assertIsArray($version);
+    }
+}
