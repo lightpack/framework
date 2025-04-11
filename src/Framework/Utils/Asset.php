76e0f7e24aa879b8e2ff9eec4534e5bce4981a87
Pradeep Tiwari <pt21388@gmail.com>
2025-04-11 20:15:48 +0530

    asset pipeline

--- src/Framework/Utils/Asset.php
+++ src/Framework/Utils/Asset.php
@@ -34,10 +34,31 @@ class Asset
      */
     protected array $preloadModules = [];
 
+    /**
+     * Version cache for assets
+     */
+    protected array $versions = [];
+
+    /**
+     * Path to version manifest
+     */
+    protected string $manifestPath;
+
+    /**
+     * Directories to track for versioning
+     */
+    protected array $trackDirs = [
+        'css',    // Stylesheets
+        'js',     // JavaScript
+        'fonts',  // Web fonts
+        'img'     // Images
+    ];
+
     public function __construct(?string $publicPath = null)
     {
         $this->publicPath = $publicPath ?? DIR_ROOT . '/public';
         $this->baseUrl = get_env('ASSET_URL') ?? get_env('APP_URL');
+        $this->manifestPath = $this->publicPath . '/assets.json';
     }
 
     /**
@@ -46,15 +67,64 @@ class Asset
     public function url(string $path, array $options = []): string
     {
         $path = trim($path, '/ ');
-        $realPath = $this->publicPath . '/' . $path;
-
-        // Add version if file exists and versioning is enabled
-        if (file_exists($realPath) && ($options['version'] ?? true)) {
-            $version = filemtime($realPath);
-            $path .= '?v=' . $version;
+        
+        if (($options['version'] ?? true)) {
+            $version = $this->getVersion($path);
+            if ($version) {
+                $path .= '?v=' . $version;
+            }
         }
 
         return ($this->baseUrl ? rtrim($this->baseUrl, '/') : '') . '/' . $path;
+    }
+
+    /**
+     * Get version for an asset
+     */
+    protected function getVersion(string $path): ?string
+    {
+        // Load versions if not loaded
+        if (empty($this->versions)) {
+            $this->loadVersions();
+        }
+
+        return $this->versions[$path] ?? null;
+    }
+
+    /**
+     * Load versions from manifest
+     */
+    protected function loadVersions(): void
+    {
+        if (file_exists($this->manifestPath)) {
+            $this->versions = json_decode(file_get_contents($this->manifestPath), true) ?? [];
+        }
+    }
+
+    /**
+     * Generate version manifest
+     */
+    public function generateVersions(): void
+    {
+        $versions = [];
+        
+        foreach ($this->trackDirs as $dir) {
+            $path = $this->publicPath . '/' . $dir;
+            if (!is_dir($path)) continue;
+
+            $iterator = new \RecursiveIteratorIterator(
+                new \RecursiveDirectoryIterator($path)
+            );
+
+            foreach ($iterator as $file) {
+                if ($file->isFile() && !$file->isLink()) {
+                    $relativePath = str_replace($this->publicPath . '/', '', $file->getPathname());
+                    $versions[$relativePath] = (string)filemtime($file->getPathname());
+                }
+            }
+        }
+
+        file_put_contents($this->manifestPath, json_encode($versions, JSON_PRETTY_PRINT));
     }
 
     /**
@@ -136,9 +206,16 @@ class Asset
         foreach ($files as $file) {
             $url = $this->url($file);
             $ext = pathinfo($file, PATHINFO_EXTENSION);
-            $type = $this->getMimeType($ext);
-
-            header("Link: <{$url}>; rel=preload; as={$type}", false);
+            
+            // Special handling for fonts and modules
+            if (in_array($ext, ['woff', 'woff2', 'ttf'])) {
+                header("Link: <{$url}>; rel=preload; as=font; crossorigin", false);
+            } elseif ($ext === 'js' && str_contains($file, 'module')) {
+                header("Link: <{$url}>; rel=modulepreload", false);
+            } else {
+                $type = $this->getMimeType($ext);
+                header("Link: <{$url}>; rel=preload; as={$type}", false);
+            }
         }
     }
 
@@ -260,5 +337,73 @@ class Asset
         $async = !empty($options['async']) ? ' async' : '';
         
         return "<script{$type} src='{$url}'{$defer}{$async}></script>\n";
+    }
+
+    /**
+     * Download and setup Google Font
+     */
+    public function googleFont(string $family, array $weights = ['400']): self
+    {
+        $fontDir = $this->publicPath . '/fonts';
+        $cssDir = $this->publicPath . '/css';
+
+        // Create directories if they don't exist
+        if (!is_dir($fontDir)) mkdir($fontDir, 0755, true);
+        if (!is_dir($cssDir)) mkdir($cssDir, 0755, true);
+
+        // Build Google Fonts URL
+        $url = sprintf(
+            'https://fonts.googleapis.com/css2?family=%s:wght@%s&display=swap',
+            str_replace(' ', '+', $family),
+            implode(';', $weights)
+        );
+
+        // Get and parse CSS
+        $css = file_get_contents($url);
+        preg_match_all('/url\((.*?)\)/', $css, $matches);
+        
+        // Download fonts and generate CSS
+        $localCss = '';
+        foreach ($weights as $weight) {
+            // Find matching font URL for this weight
+            $fontUrl = '';
+            foreach ($matches[1] as $url) {
+                if (strpos($url, $weight . '.woff2') !== false) {
+                    $fontUrl = $url;
+                    break;
+                }
+            }
+
+            if (!$fontUrl) continue;
+
+            // Download font
+            $fontContent = file_get_contents($fontUrl);
+            $fileName = strtolower($family) . '-' . $weight . '.woff2';
+            file_put_contents($fontDir . '/' . $fileName, $fontContent);
+
+            // Generate CSS
+            $localCss .= sprintf(
+                "@font-face {\n" .
+                "    font-family: '%s';\n" .
+                "    font-weight: %s;\n" .
+                "    src: local('%s'),\n" .
+                "         url('/fonts/%s') format('woff2');\n" .
+                "    font-display: swap;\n" .
+                "}\n\n",
+                $family,
+                $weight,
+                $family,
+                $fileName
+            );
+        }
+
+        // Save CSS
+        $cssFile = $cssDir . '/fonts.css';
+        file_put_contents($cssFile, $localCss);
+
+        // Update version manifest
+        $this->generateVersions();
+
+        return $this;
     }
 }
