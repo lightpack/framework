30b3902b99c3ab2104b4d1ca9fc2f39ac303c2fd
Pradeep Tiwari <pt21388@gmail.com>
2025-05-02 16:13:32 +0530

    Refactor Captcha class to use fluent interface and instance properties

--- src/Framework/Utils/Captcha.php
+++ src/Framework/Utils/Captcha.php
@@ -4,14 +4,45 @@ namespace Lightpack\Utils;
 
 class Captcha
 {
-    public static function generate(int $width = 150, int $height = 50, string $font, ?string $text = null): string
+    private int $width = 150;
+    private int $height = 50;
+    private string $font;
+    private ?string $text = null;
+
+    public function width(int $width): self
+    {
+        $this->width = $width;
+        return $this;
+    }
+
+    public function height(int $height): self
+    {
+        $this->height = $height;
+        return $this;
+    }
+
+    public function font(string $font): self
+    {
+        $this->font = $font;
+        return $this;
+    }
+
+    public function text(string $text): self
+    {
+        $this->text = $text;
+        return $this;
+    }
+
+    public function generate(): string
     {
         if (!extension_loaded('gd')) {
             throw new \Exception('GD extension is required for CAPTCHA generation.');
         }
-
-        $text = $text ?? substr(str_shuffle('ABCDEFGHJKLMNPQRSTUVWXYZ23456789'), 0, 6);
-        $im = imagecreatetruecolor($width, $height);
+        if (empty($this->font)) {
+            throw new \Exception('Font path is required for CAPTCHA generation.');
+        }
+        $text = $this->text ?? substr(str_shuffle('ABCDEFGHJKLMNPQRSTUVWXYZ23456789'), 0, 6);
+        $im = imagecreatetruecolor($this->width, $this->height);
         imagesavealpha($im, true);
         $bg = imagecolorallocatealpha($im, 255, 255, 255, 0);
         imagefill($im, 0, 0, $bg);
@@ -19,23 +50,23 @@ class Captcha
         // Add random lines for noise
         for ($i = 0; $i < 6; $i++) {
             $lineColor = imagecolorallocate($im, rand(100,200), rand(100,200), rand(100,200));
-            imageline($im, rand(0, $width), rand(0, $height), rand(0, $width), rand(0, $height), $lineColor);
+            imageline($im, rand(0, $this->width), rand(0, $this->height), rand(0, $this->width), rand(0, $this->height), $lineColor);
         }
 
         // Add random dots for noise
         for ($i = 0; $i < 150; $i++) {
             $dotColor = imagecolorallocate($im, rand(100,200), rand(100,200), rand(100,200));
-            imagesetpixel($im, rand(0, $width-1), rand(0, $height-1), $dotColor);
+            imagesetpixel($im, rand(0, $this->width-1), rand(0, $this->height-1), $dotColor);
         }
 
         // Draw the text with random angle and position
         for ($i = 0; $i < strlen($text); $i++) {
             $fontSize = rand(18, 24);
             $angle = rand(-25, 25);
-            $x = (int) (10 + $i * ($width - 20) / strlen($text));
-            $y = (int) rand($height - 15, $height - 5);
+            $x = (int) (10 + $i * ($this->width - 20) / strlen($text));
+            $y = (int) rand($this->height - 15, $this->height - 5);
             $color = imagecolorallocate($im, rand(0,80), rand(0,80), rand(0,80));
-            imagettftext($im, $fontSize, $angle, $x, $y, $color, $font, $text[$i]);
+            imagettftext($im, $fontSize, $angle, $x, $y, $color, $this->font, $text[$i]);
         }
 
         // Output image to a string
