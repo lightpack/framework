45df7249204044d0579e1f29291ca6d9f160e8bf
Pradeep Tiwari <pt21388@gmail.com>
2025-05-07 11:41:46 +0530

    Refactor captcha creation into separate methods and add native captcha config options

--- src/Framework/Providers/CaptchaProvider.php
+++ src/Framework/Providers/CaptchaProvider.php
@@ -20,14 +20,52 @@ class CaptchaProvider implements ProviderInterface
             $type = $config->get('captcha.driver');
 
             return match ($type) {
-                'null'      => new NullCaptcha($request),
-                'native'    => new NativeCaptcha($request, $container->get('session')),
-                'recaptcha' => new GoogleReCaptcha($request, $config->get('captcha.recaptcha.site_key'), $config->get('captcha.recaptcha.secret_key')),
-                'turnstile' => new CloudflareTurnstile($request, $config->get('captcha.turnstile.site_key'), $config->get('captcha.turnstile.secret_key')),
+                'null'      => $this->createNullCaptcha($request),
+                'native'    => $this->createNativeCaptcha($request, $container->get('session'), $config),
+                'recaptcha' => $this->createGoogleReCaptcha($request, $config),
+                'turnstile' => $this->createCloudflareTurnstile($request, $config),
                 default     => throw new \Exception("Unknown captcha driver: {$type}"),
             };
         });
 
         $container->alias(CaptchaInterface::class, 'captcha');
+    }
+
+    private function createNullCaptcha($request)
+    {
+        return new NullCaptcha($request);
+    }
+
+    private function createNativeCaptcha($request, $session, $config)
+    {
+        $captcha = new NativeCaptcha($request, $session);
+        if ($font = $config->get('captcha.native.font')) {
+            $captcha->font($font);
+        }
+        if ($width = $config->get('captcha.native.width')) {
+            $captcha->width((int)$width);
+        }
+        if ($height = $config->get('captcha.native.height')) {
+            $captcha->height((int)$height);
+        }
+        return $captcha;
+    }
+
+    private function createGoogleReCaptcha($request, $config)
+    {
+        return new GoogleReCaptcha(
+            $request,
+            $config->get('captcha.recaptcha.site_key'),
+            $config->get('captcha.recaptcha.secret_key')
+        );
+    }
+
+    private function createCloudflareTurnstile($request, $config)
+    {
+        return new CloudflareTurnstile(
+            $request,
+            $config->get('captcha.turnstile.site_key'),
+            $config->get('captcha.turnstile.secret_key')
+        );
     }
 }
