bae49878beca223df64eea640810612a66341e8d
Pradeep Tiwari <pt21388@gmail.com>
2025-05-16 11:54:40 +0530

    fix: update alter table syntax and add namespace in MFA migration

--- src/Framework/Mfa/Migration.php
+++ src/Framework/Mfa/Migration.php
@@ -1,21 +1,33 @@
 <?php
 
+namespace Lightpack\Uploads\Migration;
+
 use Lightpack\Database\Schema\Table;
 use Lightpack\Database\Migrations\Migration;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
     public function up(): void
     {
-        $this->alter('users', function (Table $table) {
+        $this->alter('users')->add(function (Table $table) {
             $table->varchar('mfa_method', 32)->nullable(); // User's chosen MFA factor
             $table->boolean('mfa_enabled')->default(false); // Optional: for opt-in scenarios
         });
     }
 
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
     public function down(): void
     {
-        $this->alter('users', function (Table $table) {
+        $this->alter('users')->modify(function (Table $table) {
             $table->dropColumn('mfa_method');
             $table->dropColumn('mfa_enabled');
         });
