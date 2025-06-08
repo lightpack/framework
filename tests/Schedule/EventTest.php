b2532139c97fc22b252abaf5faf1c68dac18f2e6
Pradeep Tiwari <pt21388@gmail.com>
2025-06-09 00:20:14 +0530

    feat: add helper methods for common cron schedule patterns with tests

--- tests/Schedule/EventTest.php
+++ tests/Schedule/EventTest.php
@@ -122,4 +122,116 @@ class ScheduleEventTest extends TestCase
         $this->assertInstanceOf(DateTime::class, $previousdueAt);
         $this->assertEquals($nextDueDateTime, $previousdueAt);
     }
+
+    public function testDailyHelperSetsCorrectCron()
+    {
+        $event = new Event('job', 'Test');
+        $event->daily();
+        $this->assertEquals('0 0 * * *', $event->getCronExpression());
+    }
+
+    public function testHourlyHelperSetsCorrectCron()
+    {
+        $event = new Event('job', 'Test');
+        $event->hourly();
+        $this->assertEquals('0 * * * *', $event->getCronExpression());
+    }
+
+    public function testWeeklyHelperSetsCorrectCron()
+    {
+        $event = new Event('job', 'Test');
+        $event->weekly();
+        $this->assertEquals('0 0 * * 0', $event->getCronExpression());
+    }
+
+    public function testMonthlyHelperSetsCorrectCron()
+    {
+        $event = new Event('job', 'Test');
+        $event->monthly();
+        $this->assertEquals('0 0 1 * *', $event->getCronExpression());
+    }
+
+    public function testEveryMinutesHelperSetsCorrectCron()
+    {
+        $event = new Event('job', 'Test');
+        $event->everyMinutes(15);
+        $this->assertEquals('*/15 * * * *', $event->getCronExpression());
+    }
+
+    public function testEveryMinutesHelperEdgeCaseOneMinute()
+    {
+        $event = new Event('job', 'Test');
+        $event->everyMinutes(1);
+        $this->assertEquals('*/1 * * * *', $event->getCronExpression());
+    }
+
+    public function testDailyAtHelperSetsCorrectCron()
+    {
+        $event = new Event('job', 'Test');
+        $event->dailyAt('13:45');
+        $this->assertEquals('45 13 * * *', $event->getCronExpression());
+    }
+
+    public function testMondaysHelperSetsCorrectCron()
+    {
+        $event = new Event('job', 'Test');
+        $event->mondays();
+        $this->assertEquals('0 0 * * 1', $event->getCronExpression());
+    }
+
+    public function testTuesdaysHelperSetsCorrectCron()
+    {
+        $event = new Event('job', 'Test');
+        $event->tuesdays();
+        $this->assertEquals('0 0 * * 2', $event->getCronExpression());
+    }
+
+    public function testWednesdaysHelperSetsCorrectCron()
+    {
+        $event = new Event('job', 'Test');
+        $event->wednesdays();
+        $this->assertEquals('0 0 * * 3', $event->getCronExpression());
+    }
+
+    public function testThursdaysHelperSetsCorrectCron()
+    {
+        $event = new Event('job', 'Test');
+        $event->thursdays();
+        $this->assertEquals('0 0 * * 4', $event->getCronExpression());
+    }
+
+    public function testFridaysHelperSetsCorrectCron()
+    {
+        $event = new Event('job', 'Test');
+        $event->fridays();
+        $this->assertEquals('0 0 * * 5', $event->getCronExpression());
+    }
+
+    public function testSaturdaysHelperSetsCorrectCron()
+    {
+        $event = new Event('job', 'Test');
+        $event->saturdays();
+        $this->assertEquals('0 0 * * 6', $event->getCronExpression());
+    }
+
+    public function testSundaysHelperSetsCorrectCron()
+    {
+        $event = new Event('job', 'Test');
+        $event->sundays();
+        $this->assertEquals('0 0 * * 0', $event->getCronExpression());
+    }
+
+    public function testMonthlyOnHelperSetsCorrectCron()
+    {
+        $event = new Event('job', 'Test');
+        $event->monthlyOn(15, '09:00');
+        $this->assertEquals('0 9 15 * *', $event->getCronExpression());
+    }
+    
+    public function testMonthlyOnHelperDefaultTime()
+    {
+        $event = new Event('job', 'Test');
+        $event->monthlyOn(10);
+        $this->assertEquals('0 0 10 * *', $event->getCronExpression());
+    }
 }
