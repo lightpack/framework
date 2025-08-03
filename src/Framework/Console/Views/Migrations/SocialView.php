cd63264608b7fd27575321921a7f348aa2332541
Pradeep Tiwari <pt21388@gmail.com>
2025-08-03 20:53:34 +0530

    feat: add social accounts migration template with user foreign key and indexes

--- src/Framework/Console/Views/Migrations/SocialView.php
+++ src/Framework/Console/Views/Migrations/SocialView.php
@@ -7,7 +7,33 @@ class SocialView
     public static function getTemplate()
     {
         return <<<'TEMPLATE'
+<?php
 
+use Lightpack\Database\Schema\Table;
+use Lightpack\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        $this->create('social_accounts', function (Table $table) {
+            $table->id();
+            $table->column('user_id')->type('BIGINT')->attribute('UNSIGNED');
+            $table->varchar('provider', 20);
+            $table->varchar('provider_id', 255);
+            $table->timestamps();
+
+            $table->foreignKey('user_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->unique(['provider', 'provider_id']);
+            $table->index('user_id');
+        });
+    }
+
+    public function down(): void
+    {
+        $this->drop('social_accounts');
+    }
+};
 TEMPLATE;
     }
 }
