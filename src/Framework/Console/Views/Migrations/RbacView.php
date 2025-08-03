54068d458112cc24beb7bc4fba61c40a3df1fcd9
Pradeep Tiwari <pt21388@gmail.com>
2025-08-03 20:20:13 +0530

    feat: add RBAC migration template with roles and permissions tables

--- src/Framework/Console/Views/Migrations/RbacView.php
+++ src/Framework/Console/Views/Migrations/RbacView.php
@@ -7,7 +7,55 @@ class RbacView
     public static function getTemplate()
     {
         return <<<'TEMPLATE'
+<?php
 
+use Lightpack\Database\Migrations\Migration;
+use Lightpack\Database\Schema\Table;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        $this->create('roles', function (Table $table) {
+            $table->id();
+            $table->varchar('name')->unique();
+            $table->timestamps();
+        });
+
+        $this->create('permissions', function (Table $table) {
+            $table->id();
+            $table->varchar('name')->unique();
+            $table->varchar('description')->nullable();
+            $table->timestamps();
+        });
+
+        $this->create('role_user', function (Table $table) {
+            $table->column('role_id')->type('bigint')->attribute('unsigned');
+            $table->column('user_id')->type('bigint')->attribute('unsigned');
+            $table->timestamps();
+            $table->foreignKey('role_id')->cascadeOnDelete();
+            $table->foreignKey('user_id')->cascadeOnDelete();
+            $table->primary(['role_id', 'user_id']);
+        });
+
+        $this->create('permission_role', function (Table $table) {
+            $table->column('permission_id')->type('bigint')->attribute('unsigned');
+            $table->column('role_id')->type('bigint')->attribute('unsigned');
+            $table->timestamps();
+            $table->foreignKey('permission_id')->cascadeOnDelete();
+            $table->foreignKey('role_id')->cascadeOnDelete();
+            $table->primary(['permission_id', 'role_id']);
+        });
+    }
+
+    public function down(): void
+    {
+        $this->drop('permission_role');
+        $this->drop('role_user');
+        $this->drop('permissions');
+        $this->drop('roles');
+    }
+};
 TEMPLATE;
     }
 }
