aeaf4cae79b6f7d8778386633f1ca47a41965185
Pradeep Tiwari <pt21388@gmail.com>
2025-08-03 20:54:40 +0530

    feat: add migration template for tags and polymorphic tag_models tables

--- src/Framework/Console/Views/Migrations/TagsView.php
+++ src/Framework/Console/Views/Migrations/TagsView.php
@@ -7,7 +7,37 @@ class TagsView
     public static function getTemplate()
     {
         return <<<'TEMPLATE'
+<?php
 
+use Lightpack\Database\Schema\Table;
+use Lightpack\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        $this->create('tags', function (Table $table) {
+            $table->id();
+            $table->varchar('name', 150)->unique();
+            $table->varchar('slug', 150)->unique();
+            $table->timestamps();
+        });
+
+        $this->create('tag_models', function (Table $table) {
+            $table->column('tag_id')->type('bigint')->attribute('unsigned');
+            $table->column('model_id')->type('bigint')->attribute('unsigned');
+            $table->varchar('model_type', 191);
+            $table->primary(['tag_id', 'model_id', 'model_type']);
+            $table->foreignKey('tag_id')->references('id')->on('tags')->cascadeOnDelete();
+        });
+    }
+
+    public function down(): void
+    {
+        $this->drop('tag_models');
+        $this->drop('tags');
+    }
+};
 TEMPLATE;
     }
 }
