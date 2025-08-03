2b1af6c09769ceb1960d8ca68651f64c0ee72b71
Pradeep Tiwari <pt21388@gmail.com>
2025-08-03 20:22:57 +0530

    feat: add migration template for cache table schema

--- src/Framework/Console/Views/Migrations/CacheView.php
+++ src/Framework/Console/Views/Migrations/CacheView.php
@@ -7,7 +7,28 @@ class CacheView
     public static function getTemplate()
     {
         return <<<'TEMPLATE'
+<?php
 
+use Lightpack\Database\Schema\Table;
+use Lightpack\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        $this->create('cache', function (Table $table) {
+            $table->varchar('`key`', 255)->primary();
+            $table->column('value')->type('longtext');
+            $table->column('expires_at')->type('int')->attribute('UNSIGNED');
+            $table->index('expires_at', 'idx_cache_expiry');
+        });
+    }
+
+    public function down(): void
+    {
+        $this->drop('cache');
+    }
+};
 TEMPLATE;
     }
 }
