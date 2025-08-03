44e819db020f22b06420b607802321fcac22f77c
Pradeep Tiwari <pt21388@gmail.com>
2025-08-03 13:05:25 +0530

    refactor: use imported Input class instead of fully qualified namespace in tests

--- tests/Console/InputTest.php
+++ tests/Console/InputTest.php
@@ -1,12 +1,13 @@
 <?php
 
+use Lightpack\Console\Input;
 use PHPUnit\Framework\TestCase;
 
 class InputTest extends TestCase
 {
     public function testArgumentsAreParsedCorrectly()
     {
-        $input = new \Lightpack\Console\Input(['script.php', 'foo', 'bar']);
+        $input = new Input(['script.php', 'foo', 'bar']);
         $this->assertEquals('foo', $input->getArgument(0));
         $this->assertEquals('bar', $input->getArgument(1));
         $this->assertNull($input->getArgument(2));
@@ -15,7 +16,7 @@ class InputTest extends TestCase
 
     public function testLongOptionsAreParsedCorrectly()
     {
-        $input = new \Lightpack\Console\Input(['script.php', '--alpha=1', '--beta=2', '--flag']);
+        $input = new Input(['script.php', '--alpha=1', '--beta=2', '--flag']);
         $this->assertEquals('1', $input->getOption('alpha'));
         $this->assertEquals('2', $input->getOption('beta'));
         $this->assertTrue($input->getOption('flag'));
@@ -26,7 +27,7 @@ class InputTest extends TestCase
 
     public function testShortOptionsAreParsedCorrectly()
     {
-        $input = new \Lightpack\Console\Input(['script.php', '-a', '-b', '-c', 'foo']);
+        $input = new Input(['script.php', '-a', '-b', '-c', 'foo']);
         $this->assertTrue($input->getOption('a'));
         $this->assertTrue($input->getOption('b'));
         $this->assertEquals('foo', $input->getOption('c'));
@@ -35,13 +36,13 @@ class InputTest extends TestCase
 
     public function testShortOptionWithValue()
     {
-        $input = new \Lightpack\Console\Input(['script.php', '-f', 'bar']);
+        $input = new Input(['script.php', '-f', 'bar']);
         $this->assertEquals('bar', $input->getOption('f'));
     }
 
     public function testMultipleShortFlagsTogether()
     {
-        $input = new \Lightpack\Console\Input(['script.php', '-abc']);
+        $input = new Input(['script.php', '-abc']);
         $this->assertTrue($input->getOption('a'));
         $this->assertTrue($input->getOption('b'));
         $this->assertTrue($input->getOption('c'));
@@ -49,19 +50,19 @@ class InputTest extends TestCase
 
     public function testRepeatedLongOptionsAreArrays()
     {
-        $input = new \Lightpack\Console\Input(['script.php', '--tag=foo', '--tag=bar', '--tag=baz']);
+        $input = new Input(['script.php', '--tag=foo', '--tag=bar', '--tag=baz']);
         $this->assertEquals(['foo', 'bar', 'baz'], $input->getOption('tag'));
     }
 
     public function testRepeatedShortOptionsAreArrays()
     {
-        $input = new \Lightpack\Console\Input(['script.php', '-t', 'foo', '-t', 'bar']);
+        $input = new Input(['script.php', '-t', 'foo', '-t', 'bar']);
         $this->assertEquals(['foo', 'bar'], $input->getOption('t'));
     }
 
     public function testRequiredArgumentValidation()
     {
-        $input = new \Lightpack\Console\Input(['script.php', 'foo']);
+        $input = new Input(['script.php', 'foo']);
         $input->requireArgument(0, 'First argument');
         $input->requireArgument(1, 'Second argument');
         $errors = $input->validate();
@@ -71,7 +72,7 @@ class InputTest extends TestCase
 
     public function testRequiredOptionValidation()
     {
-        $input = new \Lightpack\Console\Input(['script.php', '--foo=bar']);
+        $input = new Input(['script.php', '--foo=bar']);
         $input->requireOption('foo', 'Foo option');
         $input->requireOption('baz', 'Baz option');
         $errors = $input->validate();
@@ -81,7 +82,7 @@ class InputTest extends TestCase
 
     public function testGetUsage()
     {
-        $input = new \Lightpack\Console\Input(['script.php']);
+        $input = new Input(['script.php']);
         $input->requireArgument(0, 'First argument');
         $input->requireOption('foo', 'Foo option');
         $usage = $input->getUsage('my:command');
@@ -90,13 +91,13 @@ class InputTest extends TestCase
 
     public function testBooleanFlag()
     {
-        $input = new \Lightpack\Console\Input(['script.php', '--verbose']);
+        $input = new Input(['script.php', '--verbose']);
         $this->assertTrue($input->getOption('verbose'));
     }
 
     public function testEdgeCases()
     {
-        $input = new \Lightpack\Console\Input(['script.php', '--foo', '-b', 'bar', 'baz', '-x']);
+        $input = new Input(['script.php', '--foo', '-b', 'bar', 'baz', '-x']);
         $this->assertTrue($input->getOption('foo'));
         $this->assertEquals('bar', $input->getOption('b'));
         $this->assertTrue($input->getOption('x'));
