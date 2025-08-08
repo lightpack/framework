38e8ce412d6a43e9eae548c9afc78d402a57f129
Pradeep Tiwari <pt21388@gmail.com>
2025-08-08 09:48:55 +0530

    feat: add CorsController to handle preflight OPTIONS requests for API endpoints

---
+++ src/Framework/Controllers/CorsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Lightpack\Controllers;
+
+class CorsController
+{
+    public function index()
+    {
+        /**
+         * Nothing to do here.
+         * 
+         * When browser sends preflight request with OPTIONS method
+         * to your APIs, Lightpack\Filters\CorsFilter is invoked by the 
+         * framework itself.
+         * 
+         * The framework will match all route requests that start with
+         * '/api' prefix and HTTP method 'OPTIONS' and will call the 
+         * cors filter class to check the request headers to verify if
+         * the cross domain API request is allowed.
+         * 
+         * It also sets the appropriate response headers with CORS 
+         * configurations defined in 'config/cors.php' file.
+         * 
+         * There is an excellent article about CORS requests that you
+         * may consult for guidelines.
+         * 
+         * https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+         */
+    }
+}
