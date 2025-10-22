9f830689da7c8b5ec489bdea44a7e2f7bd977f9a
Pradeep Tiwari <pt21388@gmail.com>
2025-10-22 17:04:13 +0530

    feat: add layout and stack management to template engine

--- src/Framework/View/Template.php
+++ src/Framework/View/Template.php
@@ -9,6 +9,10 @@ class Template
     protected $data = [];
     protected $embeddedTemplate = null;
     protected string $viewsPath;
+    protected $layoutFile = null;
+    protected $childContent = null;
+    protected $stacks = [];
+    protected $currentStack = null;
 
     public function __construct(?string $viewsPath = null)
     {
@@ -33,6 +37,8 @@ class Template
     }
 
     /**
+     * @deprecated Use layout() and content() instead.
+     * 
      * Render the embedded child template if __embed is set, 
      * else return empty string.
      */
@@ -64,18 +70,68 @@ class Template
         return $this->include($file, $data);
     }
 
-    public function includeIf(bool $flag, string $file, array $data = []): string
-    {
-        if ($flag) {
-            return $this->include($file, $data);
-        }
-
-        return '';
-    }
-
     public function component(string $file, array $data = []): string
     {
         return $this->renderTemplateWithData($file, $data);
+    }
+
+    /**
+     * Declare a layout file for the current template.
+     * The child template content will be embedded in the layout.
+     */
+    public function layout(string $file): void
+    {
+        $this->layoutFile = $file;
+    }
+
+    /**
+     * Render the child template content within a layout.
+     * This method is called from within layout files.
+     */
+    public function content(): string
+    {
+        return $this->childContent ?? '';
+    }
+
+    /**
+     * Start pushing content to a stack.
+     */
+    public function push(string $stack): void
+    {
+        $this->currentStack = $stack;
+        ob_start();
+    }
+
+    /**
+     * Stop pushing content and save it to the stack.
+     */
+    public function endPush(): void
+    {
+        if ($this->currentStack === null) {
+            throw new \RuntimeException('Cannot call endPush() without a corresponding push()');
+        }
+
+        $content = ob_get_clean();
+        $stack = $this->currentStack;
+        $this->currentStack = null;
+
+        if (!isset($this->stacks[$stack])) {
+            $this->stacks[$stack] = [];
+        }
+
+        $this->stacks[$stack][] = $content;
+    }
+
+    /**
+     * Render all content that has been pushed to a stack.
+     */
+    public function stack(string $stack): string
+    {
+        if (!isset($this->stacks[$stack])) {
+            return '';
+        }
+
+        return implode("\n", $this->stacks[$stack]);
     }
 
     protected function throwExceptionIfTemplateNotFound(string $template)
@@ -98,6 +154,10 @@ class Template
 
     protected function renderTemplateWithData(string $file, array $data): string
     {
+        // Save layout state for nested renders (includes/components)
+        $savedLayoutFile = $this->layoutFile;
+        $this->layoutFile = null;
+        
         $obLevel = ob_get_level();
         ob_start();
 
@@ -107,7 +167,21 @@ class Template
             $this->catchViewExceptions($e, $obLevel);
         }
 
-        return ob_get_clean();
+        $output = ob_get_clean();
+
+        // Check if this template declared a layout
+        $declaredLayout = $this->layoutFile;
+        
+        // Restore saved layout state
+        $this->layoutFile = $savedLayoutFile;
+
+        // If a layout was declared, render it with the current output as content
+        if ($declaredLayout) {
+            $this->childContent = $output;
+            return $this->renderTemplateWithData($declaredLayout, $data);
+        }
+
+        return $output;
     }
 
     protected function requireTemplate(string $file, array $data)
