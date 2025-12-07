dcd7f257080a0f11e7236ca1d940a1f5bcf1f504
Pradeep Tiwari <pt21388@gmail.com>
2025-12-07 22:13:15 +0530

    ``` feat: add --tenant flag to model generator for automatic multi-tenancy support
    - Added --tenant flag to CreateModel command for generating TenantModel subclasses
    - Added --tenant-column flag to customize tenant column name (defaults to 'tenant_id')
    - Created getTenantTemplate() method in ModelView with TenantModel base class
    - Updated parseArguments() to extract isTenant and tenantColumn options
    - Modified writeModelFile() to conditionally use tenant template with __TENANT_COLUMN__ placeholder

--- src/Framework/Console/Views/ModelView.php
+++ src/Framework/Console/Views/ModelView.php
@@ -31,4 +31,34 @@ class __MODEL_NAME__ extends Model
 }
 TEMPLATE;
     }
+
+    public static function getTenantTemplate()
+    {
+        return <<<'TEMPLATE'
+<?php
+
+namespace __NAMESPACE__;
+
+use Lightpack\Database\Lucid\TenantModel;
+
+class __MODEL_NAME__ extends TenantModel
+{
+    protected $table = '__TABLE_NAME__';
+
+    protected $primaryKey = '__PRIMARY_KEY__';
+
+    protected $tenantColumn = '__TENANT_COLUMN__';
+
+    protected $timestamps = false;
+
+    protected $strictMode = true;
+
+    protected $hidden = [];
+
+    protected $casts = [];
+
+    protected $allowedLazyRelations = [];
+}
+TEMPLATE;
+    }
 }
